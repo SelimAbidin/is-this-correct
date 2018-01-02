efcc27207c096c1ed4190fb1ee62108e2d70bc91
Yavuz Selim Abidin <ysabidin@gmail.com>
2018-01-02 14:38:30 +0300

    test, function detection

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 
 
-// attribution https://stackoverflow.com/a/46181/1345303
-function isStringEmail(string) {
 
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(string.toLowerCase());
+var bundle = {
+      
+      // attribution https://stackoverflow.com/a/46181/1345303
+      isStringEmail : function (string) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(string.toLowerCase());
+      },
 
-}
+      email : function (value){
+            return isStringEmail(value)
+      },
 
-function email(value){
-      return isStringEmail(value)
-}
+      /**
+       * if param is undefined or null will return false
+       * if param is lower than 6 will return false
+       * if param is bigger than 5 or equal to 6, will return true
+       * @param {*} value will be converted to string if not a string (no value check)
+       * @return {Boolean} 
+       */
+      least6Char : function(value){
 
-/**
- * if param is undefined or null will return false
- * if param is lower than 6 will return false
- * if param is bigger than 5 or equal to 6, will return true
- * @param {*} value will be converted to string if not a string (no value check)
- * @return {Boolean} 
- */
-function least6Char(value){
+            if(value !== undefined && value !== null) {
+                  return value.toString().length >= 6
+            }
+            return false
+      },
 
-      if(value !== undefined && value !== null) {
-            return value.toString().length >= 6
+      isFunction : function (value) {
+            return typeof value === 'function'
       }
-      return false
+
 }
 
 
-export {
-        isStringEmail,
-        email,
-        least6Char
-      }
+module.exports = bundle
+
+// if (typeof module === 'object' && module.exports) {
+//       module.exports = bundle
+// } else { // UML
+      
+//       export {
+//             isStringEmail,
+//             email,
+//             least6Char
+//           } from bundle
+
+// }
+
---
+++ test.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var {isStringEmail, isFunction, least6Char} = require('./')
+
+
+describe('isStringEmail', function() { 
+
+      it('String should be mail : ysabidin@gmail.com ', function() {
+            assert.equal(true, isStringEmail('ysabidin@gmail.com'))
+      })
+
+      it('String should not be mail : ysa bidin@gmail.com ', function() {
+            assert.equal(false, isStringEmail("ysa bidin@gmail.com"))
+      })
+
+      it('String should not be mail : ysabidin@gmailcom ', function() {
+            assert.equal(false, isStringEmail("ysabidin@gmailcom"));
+      })
+
+      it('String should be mail : ysabidin@gmail.com.tr ', function() {
+            assert.equal(true, isStringEmail("ysabidin@gmail.com.tr"));
+      })
+
+})
+
+
+
+describe('least6Char', function() { 
+
+      it('Mustbe 6 char : "123456" ', function() {
+            assert.equal(true, least6Char("123456"))
+      })
+
+      it('Number must be 6 char : 123456 ', function() {
+            assert.equal(true, least6Char(123456))
+      })
+
+      it('Must not be 6 char : "12345" ', function() {
+            assert.equal(false, least6Char("12345"))
+      })
+
+      it('Number must be 6 char : 12345 ', function() {
+            assert.equal(false, least6Char(12345))
+      })
+   
+})
+
+
+describe('isFunction', function() { 
+
+      var a = function () {}
+
+      it('Must be a functon ', function() {
+            assert.equal(true, isFunction(a))
+      })
+
+      it('undefined Must not be a functon ', function() {
+            assert.equal(false, isFunction(undefined))
+      })
+
+      it('null Must not be a functon ', function() {
+            assert.equal(false, isFunction(null))
+      })
+
+      it('12 Must not be a functon ', function() {
+            assert.equal(false, isFunction(12))
+      })
+   
+})
